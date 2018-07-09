abecef0d170d572143aea93d3d07ed1e32cd48fe
aiyane <2310091880qq@gmail.com>
2018-07-09 18:25:54 +0800

    新增-用户登录,注册,忘记密码,密码重置等操作的框架

---
+++ TS_WHUT/operation/forms.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# forms.py
+"""
+模块功能: 表单
+"""
+__author__ = 'Aiyane'
+from django import forms
+
+
+class LoginForm(forms.Form):
+    """
+    这是一个登录表单(LoginForm)的类，该类继承于Form类，
+    接受一个POST网页请求(request.POST)参数，该类有一个is_valid方法，
+    用于检验Key对应值得基本格式是否出错，若出错则返回False，
+    无误则返回True.以下方法中Key必须与前端传过来的Key相同
+    """
+    username = forms.CharField(required=True, min_length=2, max_length=10)  # 这里的True指的是如果这个字段为空会报错
+    password = forms.CharField(required=True, min_length=5, max_length=16)
+
+
+class RegisterForm(forms.Form):
+    """
+    这是一个注册表单(RegisterForm)的类，该类继承于Form类，
+    接受一个POST网页请求(request.POST)参数，该类有一个is_valid方法，
+    用于检验Key对应值得基本格式是否出错，若出错则返回False，
+    无误则返回True。
+    """
+    email = forms.EmailField(required=True)
+    username = forms.CharField(required=True, min_length=2, max_length=10)
+    password = forms.CharField(required=True, min_length=5, max_length=16)
+
+
+class ModifyPwdForm(forms.Form):
+    password1 = forms.CharField(required=True, min_length=5, max_length=16)
+    password2 = forms.CharField(required=True, min_length=5, max_length=16)
--- TS_WHUT/operation/models.py
+++ TS_WHUT/operation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import UserProfile
+from datetime import datetime  # 导入当前时间
 
-# Create your models here.
+
+class UserMessage(models.Model):
+    # 网站用户信息
+    post_user = models.CharField(max_length=20, default="图说理工网", verbose_name="发送用户")
+    user = models.ForeignKey(UserProfile, models.CASCADE ,verbose_name="接收用户")
+    message = models.CharField(max_length=500, verbose_name=u"消息内容")
+    has_read = models.BooleanField(default=False, verbose_name=u"是否已读")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"用户消息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.message
