9723382bffe8dda5eb4995802235dca0ac0a5b0e
aiyane <2310091880qq@gmail.com>
2018-07-16 17:57:40 +0800

    新增-xadmin模块

--- TS_WHUT/TS_WHUT/urls.py
+++ TS_WHUT/TS_WHUT/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from Users.views import Index
 from django.views.generic.base import TemplateView
+import xadmin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),  # 主页
     path('users/', include('Users.urls')),  # 配置用户页url
     path('operation/', include('operation.urls')),  # 配置操作页url
