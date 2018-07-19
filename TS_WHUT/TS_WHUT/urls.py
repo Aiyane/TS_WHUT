e3ee3a78ffb654717bd3be58623765b7223f2022
liaohucheng <liao_hucheng@163.com>
2018-07-19 16:01:43 +0800

    完成xadmin管理后台的配置

--- TS_WHUT/TS_WHUT/urls.py
+++ TS_WHUT/TS_WHUT/urls.py
@@ -13,17 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import TemplateView
-
-from django.conf.urls import url
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve
-
 import xadmin
-from operation.views import ActiveUserView, ResetView,  ModifyPwdView, CatesView
+from operation.views import ActiveUserView, CatesView
 from Users.views import (RegisterView, LoginView, GetUserMsgView,
                          LogoutView, History, FollowView, IsLogin, Following, FollowNum, FanNum)
 from Images.views import ImageView, ImageCateView, ImagePattern, ImageUser, ImageLike, ImageCollect
