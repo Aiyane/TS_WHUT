94077a71d0923e8ff6d245b46559fca561990db9
whutlhc <liao_hucheng@163.com>
2018-07-26 10:06:20 +0800

    新增-检索模块，利用haystack框架，whoosh搜索引擎和jieba分词工具，因为它原本的分词只针对英文，对中文的分词效果不好，所以就用结巴分词替换。

---
+++ TS_WHUT/Users/search_indexes.py
@@ -0,0 +1,12 @@
+from haystack import indexes
+from .models import ImageModel
+
+class ImageModelIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, using_tamplate=True)
+
+    def get_model(self):
+        return ImageModel
+
+    def index_queryset(self, using=None):
+
+    return self.get_model().objects().all()
